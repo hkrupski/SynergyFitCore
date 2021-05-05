2e75b08f20728367a123aba883b08c8ca61bc62c
hkrupski <83681459+hkrupski@users.noreply.github.com>
2021-05-04 21:24:45 -0400

    Add files via upload

---
+++ SynergyFitCore/SynergyFitCore/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace SynergyFitCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Initiating Class test: ");
+            Console.WriteLine("\nCreating SleepData");
+
+            SleepData sleepData = new SleepData("mySleepData");
+
+            Console.WriteLine("\nStarting a sensor loop to gather random data for 10 seconds:");
+            sleepData.StartSensorLoop();
+
+            DateTime firstDateTime = DateTime.Now;
+            DateTime secondDateTime = DateTime.Now;
+
+            for (int i = 10; i > 0; i = i - 1)
+            {
+                Console.WriteLine(i);
+                if (i == 7)
+                    firstDateTime = DateTime.Now;
+                if (i == 3)
+                    secondDateTime = DateTime.Now;
+                Thread.Sleep(1000);
+            }
+
+            Console.WriteLine("\nEnding Sensor loop");
+            sleepData.EndSensorLoop();
+
+            Console.WriteLine("\nPrinting all Data:");
+
+            foreach (var item in sleepData.DataDictionary)
+            {
+                Console.WriteLine(item.Key + ": " + item.Value);
+            }
+
+
+            Console.WriteLine("\nPrinting Data from " + firstDateTime.ToString() + " to " + secondDateTime.ToString());
+
+            Dictionary<DateTime, int> dict = sleepData.ReturnDataRequest(firstDateTime, secondDateTime);
+
+            foreach (var item in dict)
+            {
+                Console.WriteLine(item.Key + ": " + item.Value);
+            }
+
+        }
+    }
+}
---
+++ SynergyFitCore/SynergyFitCore/SleepData.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SynergyFitCore
+{
+    public class SleepData
+    {
+        private string _displayName;
+        private Dictionary<DateTime, int> _dataDictionary;
+        private bool _endSensor;
+
+        public SleepData(string displayName)
+        {
+            _displayName = displayName;
+            _dataDictionary = new Dictionary<DateTime, int>();
+        }
+
+        public string DisplayName
+        {
+            get
+            {
+                return _displayName;
+            }
+            set
+            {
+                _displayName = value;
+            }
+        }
+
+        public Dictionary<DateTime, int> DataDictionary
+        {
+            get
+            {
+                return _dataDictionary;
+            }
+            set
+            {
+                _dataDictionary = value;
+            }
+        }
+
+        public void StartSensorLoop()
+        {
+            _endSensor = false;
+            Task.Run(() =>
+            {
+                while (_endSensor == false)
+                {
+                    Thread.Sleep(1000);
+                    DataDictionary.Add(DateTime.Now, new Random().Next());
+                }
+            });
+        }
+
+        public void EndSensorLoop()
+        {
+            _endSensor = true;
+        }
+
+        public Dictionary<DateTime, int> ReturnDataRequest(DateTime fromDate, DateTime toDate)
+        {
+            Dictionary<DateTime, int> keyValuePairs = new Dictionary<DateTime, int>();
+            foreach (var item in DataDictionary)
+            {
+                if (item.Key.Ticks > fromDate.Ticks && item.Key.Ticks < toDate.Ticks)
+                {
+                    keyValuePairs.Add(item.Key, item.Value);
+                }
+            }
+
+            return keyValuePairs;
+        }
+
+    }
+}
